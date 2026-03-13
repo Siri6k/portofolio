153e40d0db46608bdfa1ca47cd1c7acc8be7848d
sirisk <adamchrisk@gmail.com>
2026-03-13 14:27:54 +0200

    add 3 projects

--- src/components/common/Constants.js
+++ src/components/common/Constants.js
@@ -4,10 +4,38 @@ import markdownImage from "../../assets/fcc-md.png";
 import quoteMachineImage from "../../assets/fcc-qm.png";
 import amznClone from "../../assets/niplan.png";
 import ethScan from "../../assets/eth-scan.png";
+import npMarket from "../../assets/np-market.png";
+import codAdmin from "../../assets/cod-admin.png";
+import codStore from "../../assets/cod-landing.png";
 
 export const myProjects = [
   {
     key: "1",
+    title: "Niplan Market project",
+    content:
+      "Niplan Market est une plateforme congolaise pour acheter et vendre en ligne en RDC. Livraison rapide, vendeurs locaux et paiement sécurisés. Découvrez une expérience d'achat pratique et fiable avec Niplan Market.",
+    image: npMarket,
+    link: "https://niplan-market.vercel.app",
+  },
+
+  {
+    key: "2",
+    title: "Cash on Delivery Admin Dashboard",
+    content:
+      "Cash on Delivery Admin Dashboard is a web application designed to manage and track cash on delivery orders efficiently. It provides features such as order management, inventory tracking, and real-time analytics to streamline the cash on delivery process for businesses.",
+    image: codAdmin,
+    link: "https://cash-on-delivery-admin-page.vercel.app/",
+  },
+  {
+    key: "3",
+    title: "Cash on Delivery Store Dashboard",
+    content:
+      "Cash on Delivery Store Dashboard is a web application designed to help customers order and track their cash on delivery purchases.",
+    image: codStore,
+    link: "https://cash-on-delivery-store.vercel.app/",
+  },
+  {
+    key: "4",
     title: "Amazon Inventory project",
     content:
       "Amazon inventory clone project using React, Redux, Django, SQL, and AWS S3.",
@@ -15,21 +43,21 @@ export const myProjects = [
     link: "https://niplan-market.onrender.com/",
   },
   {
-    key: "2",
+    key: "5",
     title: "Ethereum scan project",
     content: "Ethereum scan project using React and alchemy API.",
     image: ethScan,
     link: "https://blockexplorer-clone.vercel.app/",
   },
   {
-    key: "3",
+    key: "6",
     title: "A drum machine project",
     content: "A dummy project of a drum machine with freeCodeCamp.",
     image: drumMachineImage,
     link: "https://fcc-drum-machine-nu.vercel.app/",
   },
   {
-    key: "4",
+    key: "7",
     title: "A markdown previewer project",
     content:
       "A dummy project of mark down previewer that transform some text in html page with freeCodeCamp.",
@@ -38,14 +66,14 @@ export const myProjects = [
   },
 
   {
-    key: "5",
+    key: "8",
     title: "A random quote machine project",
     content: "A dummy project that generate random quotes with freeCodeCamp.",
     image: quoteMachineImage,
     link: "https://quote-machine-dun.vercel.app/",
   },
   {
-    key: "6",
+    key: "9",
     title: "25 + 5 Clock project",
     content: "It is a timer that counts down from 25 minutes and 5 minutes.",
     image: clockImage,
